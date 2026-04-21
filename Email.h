bad1d5bba3bd0f2a6733779cad0650cab46ac617
banrayndox <banrayndox@gmail.com>
2026-04-21 23:13:48 +0600

    Create Email.h

---
+++ Email.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+using namespace std;
+struct Email{
+    int id;
+    string sender;
+    string receiver;
+    string subject;
+    string body;
+    string status;
+};
